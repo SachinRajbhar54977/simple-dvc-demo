8b5711fe968471ba61cb000c376198ade5c002f4
sachin bhardwaj <sachinbhardwaj54977@gmail.com>
2026-05-06 19:50:40 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version="0.0.1",
+    description='A sample ML project',
+    author='sachin',
+    packages=find_packages(),
+    licence='MIT',
+)
